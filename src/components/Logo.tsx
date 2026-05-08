7c1c45b7cb11c6c81a95d1cb1244a753ada43e23
Eric Boom Media <eric@boommedia.us>
2026-05-08 15:55:20 -0400

    Fix logo filename: use logo.png from public folder

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,5 @@
 export default function Logo({ className = '' }: { className?: string }) {
   return (
-    <img src="/addee-online-logo-wht.png" alt="AdDee" className={`h-8 ${className}`} />
+    <img src="/logo.png" alt="AdDee" className={`h-8 ${className}`} />
   )
 }
